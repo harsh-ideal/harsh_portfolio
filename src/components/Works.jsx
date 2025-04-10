ddaf0a22bce9ff719ff9aff6f111b7354af005dc
Harsh Srivastava <harshideal2002@gmail.com>
2025-04-10 20:09:57 +0530

    Update react-tilt to react-parallax-tilt

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Tilt } from "react-tilt";
+import Tilt from 'react-parallax-tilt';
 import { motion } from "framer-motion";
 import { styles } from "../style";
 import { github } from "../assets";
@@ -44,13 +44,12 @@ const ProjectCard=({index, name, description,tags, image, source_code_link})=>{
   return(
     <motion.dev variants={fadeIn("up","spring", index*0.5, 0.75)}>
       <Tilt
-      options={{
-        max:45,
-        scale:1,
-        speed:450,
-      }}
-      className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
-      >
+  tiltMaxAngleX={45}
+  tiltMaxAngleY={45}
+  scale={1}
+  transitionSpeed={450}
+  className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+>
         <div className="relative w-full h-[230px]">
           <img src={image} alt={name} className="w-full h-full Object-cover rounded-2xl"/>
 
